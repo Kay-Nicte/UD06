a5071cebf4b03fc668f5695b67bda1b42dd0f215
Ixabel Justo <71872946+Kay-Nicte@users.noreply.github.com>
2022-04-07 18:35:13 +0200

    "Ejercicio 8"

---
+++ UD06/src/UD06/Ejercicio8App.java
@@ -0,0 +1,41 @@
+package UD06;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio8App {
+
+	public Ejercicio8App() {
+		// TODO Auto-generated constructor stub
+		
+		/*
+		 * OBJETIVO: Crear array de 10 posiciones de números pedidos por teclado
+		 * Mostrar por consola el índice y el valor al que corresponde
+		 * Hacer dos métodos:
+		 * 
+		 * 1 método: rellenar valores 
+		 * 2 método: mostrar valores
+		 */
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int i = 0;
+		int[] array=new int[10];
+			
+		//Bucle para rellenar el array de valores introducidos por parámetro
+		while (i<10) {
+		String askValue = JOptionPane.showInputDialog("Introduce otro valor: ");
+		int value = Integer.parseInt(askValue);
+		
+		//Meter el valor introducido en el array
+		array[i] = value;
+		i++;	
+		}
+		
+		//Bucle para mostrar el array
+		for (int x=0; x<=10; x++) {
+		System.out.println("Valor "+(x+1)+": "+array[x]);
+		}
+	}
+}
